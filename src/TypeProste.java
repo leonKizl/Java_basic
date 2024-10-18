89b315a4582ff82a26b7a2d1e9141bceb3740c7d
Leon Kozłowski <103368148+leonKizl@users.noreply.github.com>
2024-10-18 21:03:34 +0200

    Prymitywne typy dannych

---
+++ src/TypeProste.java
@@ -0,0 +1,17 @@
+public class TypeProste {
+    //staloprzecinkowe
+    byte fstNumb = 127; //1 bajt (-128;127)
+    short secNum = 32766; //2 bajty (-32788;32767)
+    int thNum = 3276899; //4 bajty -2 147 483 648
+    long fourNum = 222342424; // 8 bajtow
+
+    //zmiennoprzecinkowe
+    float fifthNumber = 4.9F; // 4 bajt (6 cyfr po przecinku)
+    double sixthNumb = 4.9; // 8 bajt (15 cyfr po przecin)
+
+    //wartosc logiczna
+    boolean prawda = true;
+
+    //pojedynczy znak
+    char letter = 'A';
+}
